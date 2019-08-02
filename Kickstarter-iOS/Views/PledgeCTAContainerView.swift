813035ea777e2cbb4359ae8df63763bfc259ff8d
Nino Collaco <scollaco@gmail.com>
2019-08-02 17:25:03 -0400

    Changed configureWith input to receive a Either<Project, ErrorEnvelope> type

--- Kickstarter-iOS/Views/PledgeCTAContainerView.swift
+++ Kickstarter-iOS/Views/PledgeCTAContainerView.swift
@@ -131,7 +131,7 @@ final class PledgeCTAContainerView: UIView {
 
   // MARK: - Configuration
 
-  func configureWith(value: (project: Project, isLoading: Bool)) {
+  func configureWith(value: (projectOrError: Either<Project, ErrorEnvelope>, isLoading: Bool)) {
     self.viewModel.inputs.configureWith(value: value)
   }
 
