ceb578bdef10997a54eab65f194c5c89e2095038
Nino Collaco <scollaco@gmail.com>
2019-08-08 16:27:34 -0400

    Fixed constraints on the container

--- Kickstarter-iOS/Views/Controllers/ProjectPamphletViewController.swift
+++ Kickstarter-iOS/Views/Controllers/ProjectPamphletViewController.swift
@@ -3,6 +3,12 @@ import Library
 import Prelude
 import UIKit
 
+private enum Layout {
+  enum CTAContainerView {
+    static let minHeight: CGFloat = 130
+  }
+}
+
 public protocol ProjectPamphletViewControllerDelegate: AnyObject {
   func projectPamphlet(
     _ controller: ProjectPamphletViewController,
@@ -205,7 +211,7 @@ public final class ProjectPamphletViewController: UIViewController {
   }
 
   @objc func pledgeRetryButtonTapped() {
-    self.viewModel.inputs.backThisProjectTapped()
+    self.viewModel.inputs.pledgeRetryButtonTapped()
   }
 }
 
